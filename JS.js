dab6c67679b95970c49bf468760b8c7ce6e84e86
MaHappyCode <90353237+MaHappyCode@users.noreply.github.com>
2022-10-16 01:46:04 +0200

    ++

--- JS.js
+++ JS.js
@@ -1,12 +1,11 @@
 var a;
 
 function btn_hide_show() {
-    if (a==1){
-        document.getElementById("txt2").style.display="inline";
-        return a=0;
-    } else {
-        document.getElementById("txt2").style.display="none";
-        return a=1;
-    }
+  if (a == 1) {
+    document.getElementById("txt2").style.display = "inline";
+    return (a = 0);
+  } else {
+    document.getElementById("txt2").style.display = "none";
+    return (a = 1);
+  }
 }
-
--- about.js
+++ about.js
@@ -1,11 +1,11 @@
 var a;
 
 function btn_hide_show() {
-    if (a==1){
-        document.getElementById("windo").style.display="inline";
-        return a=0;
-    } else {
-        document.getElementById("windo").style.display="none";
-        return a=1;
-    }
+  if (a == 1) {
+    document.getElementById("windo").style.display = "inline";
+    return (a = 0);
+  } else {
+    document.getElementById("windo").style.display = "none";
+    return (a = 1);
+  }
 }
